e7b28c7ac6d7c96449655978b5f0e37d9cea3bd2
cponz <cris_ponz@yahoo.es>
2017-07-23 21:31:22 +0100

    final version

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Assignment
 Gettning and Cleaning data
 
-This is my read me file
+Description of the script:
+
+1. Download zip data from the given data source and unzip the file "UCI HAR Dataset"
+2. Read files into dataframes and change column names to descriptive labels
+3. Select only the variables from features that contain a mean or std meassurement
+4. Select the data from the selected variables in step 3
+5. Merge all data into a single set including the activities names and subjects ids
+6. Calculate the average of the selected variables by activity and subjet
+7. Clean variable names
+8. Print data into txt file: tidy.txt
--- run_analysis.R
+++ run_analysis.R
@@ -8,30 +8,30 @@ unzip("./Dataset.zip")
 
 ##Read Data for the analysis
 
-##Read Features and acivities lables
-activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
-featuresLabels <- read.table("./UCI HAR Dataset/features.txt")
+   ##Read Features and acivities lables
+      activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+      featuresLabels <- read.table("./UCI HAR Dataset/features.txt")
 
-##Read observations
-##Train
-trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")
-trainSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-trainActivities <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+   ##Read observations
+      ##Train
+      trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")
+      trainSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+      trainActivities <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 
-##Rename columns for train set
-colnames(trainData) = featuresLabels[,2]
-colnames(trainSubjects) = "Subject_ID"
-colnames(trainActivities) = "Activity_ID"
+      ##Rename columns for train set
+      colnames(trainData) = featuresLabels[,2]
+      colnames(trainSubjects) = "Subject_ID"
+      colnames(trainActivities) = "Activity_ID"
 
-##Test
-testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
-testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-testActivities <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+      ##Test
+      testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
+      testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+      testActivities <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 
-##Rename columns for train set
-colnames(testData) = featuresLabels[,2]
-colnames(testSubjects) = "Subject_ID"
-colnames(testActivities) = "Activity_ID"
+      ##Rename columns for train set
+      colnames(testData) = featuresLabels[,2]
+      colnames(testSubjects) = "Subject_ID"
+      colnames(testActivities) = "Activity_ID"
 
 ##Select only the features with measurement mean and std
 selectVector <- intersect(grep("mean|std",featuresLabels$V2,value=TRUE),grep("Freq",featuresLabels$V2,invert=TRUE,value = TRUE))
@@ -64,6 +64,13 @@ meltData <- melt(mergeData, id = c("activityName", "Subject_ID"), measure.vars =
 
 ##Calculate the average of each measure by activity and individual
 finalData <- dcast(meltData,activityName + Subject_ID ~ variable, mean)
+
+##Clean the name of the variables
+newNames <- gsub("-"," ",names(finalData))
+newNames <- gsub("()","",newNames,fixed=TRUE)
+newNames <- gsub("mean", "Mean", newNames)
+newNames <- gsub("std","StnDev",newNames)
+colnames(finalData) = newNames
 
 ##Print in txt file: tidy.txt
 write.table(finalData, "./tidy.txt")
